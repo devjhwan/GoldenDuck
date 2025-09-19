e55361bd5ba8f5449c0b90f236e37f40fe9dc318
JungHwan Lee <junghwan.lee@adp.com>
2025-09-19 10:29:03 +0200

    initial commit of react page

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>Name: customer1</li>
+        <li>Name: customer1</li>
+        <li>Name: customer1</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
