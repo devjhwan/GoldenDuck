1e647bcfe33e89f76b0fcd3e4400f75e07be2701
JungHwan Lee <junghwan.lee@adp.com>
2025-09-19 10:43:41 +0200

    add static version of customer list
    Co-Authored-By: Pilar Medina <pilarmedina2000@gmail.com>
    
    Co-Authored-By: Omar Ninach <omar.ninach@adp.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,36 @@
 import './App.css'
 
+let customers = [
+    {
+      "id": 0,
+      "name": "Mary Jackson",
+      "email": "maryj@abc.com",
+      "password": "maryj"
+    },
+    {
+      "id": 1,      
+      "name": "Karen Addams",
+      "email": "karena@abc.com",
+      "password": "karena"
+    },
+    {
+      "id": 2,
+      "name": "Scott Ramsey",
+      "email": "scottr@abc.com",
+      "password": "scottr"
+    }
+  ]
+
 function App() {
   return (
     <>
+      <h1>Customer List</h1>
       <ul>
-        <li>Name: customer1</li>
-        <li>Name: customer1</li>
-        <li>Name: customer1</li>
+        {customers.map((customer) => (
+          <li key={customer.id}>
+            id: {customer.id}, name: {customer.name}, email: {customer.email}, password: {customer.password}
+          </li>
+        ))}
       </ul>
     </>
   )
